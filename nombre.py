8125c25611fcc42bb68ade8781b07f6f615e4c8e
enekoisasiugarte-hahs <enekoisasiugarte@gmail.com>
2026-04-23 12:28:45 +0200

    Despues de  guardar en Git

--- nombre.py
+++ nombre.py
@@ -5,3 +5,4 @@ direccion = "Ramon Rubial, 15"
 print(nombre)
 print("Hola, mi nombre es " + nombre + "" + apellido + " y vivo en " + direccion)
 print("Comprobación de sincronización realizada con éxito.")
+print("Despues de guardar en Git")
