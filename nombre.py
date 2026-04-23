1427b424f75308ac9b1098f9861f55c7040c06ff
Eneko <enekoisasiugarte@gmail.com>
2026-04-23 10:10:36 +0200

    coo puedes ver en la foto

--- nombre.py
+++ nombre.py
@@ -2,4 +2,5 @@ nombre = "Eneko"
 apellido = "Isasi"
 edad = 54
 direccion = "Ramon Rubial, 15"
-print("Hola, mi nombre es " + nombre + " " + apellido + " y vivo en " + direccion)
+print(nombre)
+print("Hola, mi nombre es " + nombre + "" + apellido + " y vivo en " + direccion)
