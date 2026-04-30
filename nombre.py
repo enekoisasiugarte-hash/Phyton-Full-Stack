5a959bed7522255a05399d0bb2dd6ac0f18be236
enekoisasiugarte-hahs <enekoisasiugarte@gmail.com>
2026-04-30 13:44:27 +0200

    Ejercicio IVA completado

--- nombre.py
+++ nombre.py
@@ -17,4 +17,23 @@ print(f"¿Es {nombre} mayor de edad?: {es_mayor_de_edad}")
 print(f"Su altura es de {altura} metros.")
 altura = altura + 0.05
 print(f"Si me pongo zapatos, mido {altura} metros.")
-print("Confirmación Prueba Nº1" )
+# --- Ejercicio 1: Intercambio de variables ---
+vaso_rojo = "Agua"
+vaso_azul = "Vino"
+
+print(f"Antes: Rojo tiene {vaso_rojo} y Azul tiene {vaso_azul}")
+
+# Tu reto: Intercambia los contenidos. 
+# Pista: Usa una variable llamada 'ayuda' para no derramar nada.
+
+ayuda = vaso_rojo
+vaso_rojo = vaso_azul
+vaso_azul = ayuda
+
+print(f"Después: Rojo tiene {vaso_rojo} y Azul tiene {vaso_azul}")
+print("Ejercicio 1 completado con éxito!")
+print("Ejercicio 2 completado con éxito!")  
+precio = 100
+iva = precio * 0.21
+total = precio + iva
+print(f"El total con IVA es: {total}")
