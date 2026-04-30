dbc36203c19d532bcdfe8765e2ff4bda28aa8e8a
enekoisasiugarte-hahs <enekoisasiugarte@gmail.com>
2026-04-30 12:25:13 +0200

    Confirmación Prueba Nº1

--- nombre.py
+++ nombre.py
@@ -5,19 +5,16 @@ direccion = "Ramon Rubial, 15"
 print(nombre)
 print(f"Hola, mi nombre es {nombre} {apellido} y vivo en {direccion}")
 print("Comprobación de sincronización realizada con éxito.")
-print("Despues de guardar en Git")
-print("Despues de guardar en GitHub 2ª prueba")
-print("Despues de guardar en GitHub 3ª prueba")
-print("Despues de guardar en GitHub 4ª prueba") 
-print("Despues de guardar en GitHub 5ª prueba")
-print("Despues de guardar en GitHub 6ª prueba")
-print("Despues de guardar en GitHub 7ª prueba")
-print("Despues de guardar en GitHub 8ª prueba")
-# Así quedaría en tu código:
-print(f"Hola {nombre}, tienes {edad} años.")
-año_actual = 2026
-nacimiento = año_actual - edad
-print(f"Nací aproximadamente en el año {nacimiento}")
-print("Despues de guardar en GitHub 9ª prueba") 
-print("Despues de guardar en GitHub 10ª prueba")   
-print("Despues de guardar en GitHub 11ª prueba") 
+# Variables numéricas y booleanas
+altura = 1.80          # Esto es un Float
+estudiante = True      # Esto es un Boolean (Nota la mayúscula)
+
+# Operaciones con variables
+puntuacion_total = 10 + 5
+es_mayor_de_edad = edad >= 18  # Comparación que devuelve un Boolean
+
+print(f"¿Es {nombre} mayor de edad?: {es_mayor_de_edad}")
+print(f"Su altura es de {altura} metros.")
+altura = altura + 0.05
+print(f"Si me pongo zapatos, mido {altura} metros.")
+print("Confirmación Prueba Nº1" )
